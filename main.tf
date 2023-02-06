6b8c859f04cf9ba0d97bcfe0aa26a9c505680bde
EC2 Default User <ec2-user@ip-172-31-76-48.ec2.internal>
2023-02-06 19:24:26 +0000

    changing it back to default vpc and adding egress rule

--- main.tf
+++ main.tf
@@ -2,52 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "assignvpc" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = "172.31.96.0/20"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "example-vpc"
+    Name = "public-subnet"
   }
-}
-
-resource "aws_internet_gateway" "assignigw" {
-  vpc_id = aws_vpc.assignvpc.id
-
-  tags = {
-    Name = "assign-igw"
-  }
-}
-
-resource "aws_route_table" "assign_route_table" {
-  vpc_id = aws_vpc.assignvpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.assignigw.id
-  }
-  tags = {
-    Name = "assign_route_table"
-  }
-}
-
-resource "aws_route_table_association" "public_route_table_association" {
-  route_table_id = aws_route_table.assign_route_table.id
-  subnet_id      = aws_subnet.public_subnet.id
-}
-
-resource "aws_subnet" "public_subnet" {
-vpc_id     = aws_vpc.assignvpc.id
-cidr_block = "10.0.0.0/24"
-map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
 
 resource "aws_security_group" "instance" {
   name        = "instance1-instance-sg"
   description = "Allow SSH access and http"
-  vpc_id     = aws_vpc.assignvpc.id
+  vpc_id     = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -84,23 +55,18 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
     
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
-
-# data "aws_ami" "amazon_linux" {
-#   most_recent = true
-#   owners      = ["amazon"]
-
-#   filter {
-#     name   = "name"
-#     values = ["ami-0aa7d40eeae50c9a9"]
-#   }
-# }
 
 resource "aws_instance" "instance1" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id = data.aws_vpc.default.id
   vpc_security_group_ids = [aws_security_group.instance.id]
   iam_instance_profile = "LabInstanceProfile"
   key_name = "clo835"
