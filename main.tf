ef0ffee93e02949d467a88dc13a234ba5baed286
EC2 Default User <ec2-user@ip-172-31-76-48.ec2.internal>
2023-02-06 23:11:11 +0000

    updating ports on main

--- main.tf
+++ main.tf
@@ -36,13 +36,6 @@ resource "aws_security_group" "instance" {
     }
     
   ingress {  
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-  ingress {
     from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
@@ -52,6 +45,13 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = 8082
     to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     
