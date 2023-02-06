1bfd6c43af09d8ac990491b250d0d63398cfbf1a
neilsuryanarayanan <cristianoneil39@gmail.com>
2023-02-06 17:38:42 +0000

    fixing errors, creating new vpc

--- main.tf
+++ main.tf
@@ -2,13 +2,41 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "assignvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "assignigw" {
+  vpc_id = aws_vpc.assignvpc.id
+
+  tags = {
+    Name = "assign-igw"
+  }
+}
+
+resource "aws_route_table" "assign_route_table" {
+  vpc_id = aws_vpc.assignvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.assignigw.id
+  }
+  tags = {
+    Name = "assign_route_table"
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  route_table_id = aws_route_table.assign_route_table.id
+  subnet_id      = aws_subnet.public_subnet.id
 }
 
 resource "aws_subnet" "public_subnet" {
-vpc_id     = data.aws_vpc.default.id
-cidr_block = "172.31.96.0/20"
+vpc_id     = aws_vpc.assignvpc.id
+cidr_block = "10.0.0.0/24"
 map_public_ip_on_launch = true
 
   tags = {
@@ -19,6 +47,7 @@ map_public_ip_on_launch = true
 resource "aws_security_group" "instance" {
   name        = "instance1-instance-sg"
   description = "Allow SSH access and http"
+  vpc_id     = aws_vpc.assignvpc.id
 
   ingress {
     from_port   = 22
@@ -27,7 +56,7 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
     }
     
-    ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -41,14 +70,14 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
     }
     
-   ingress {
+  ingress {
     from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     }
     
-   ingress {
+  ingress {
     from_port   = 8082
     to_port     = 8082
     protocol    = "tcp"
@@ -58,18 +87,18 @@ resource "aws_security_group" "instance" {
 }
 
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*-x86_64-gp2"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["ami-0aa7d40eeae50c9a9"]
+#   }
+# }
 
 resource "aws_instance" "instance1" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.instance.id]
